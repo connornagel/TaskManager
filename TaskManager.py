e23aa60c452f9c1ceed0e9cc5819bee967a0834c
Connor <conagelbagel586@gmail.com>
2017-06-05 00:56:52 -0700

    Init Commit

---
+++ TaskManager.py
@@ -0,0 +1,15 @@
+import kivy
+from kivy.app import App
+from kivy.uix.button import Label
+from kivy.uix.button import Button
+
+class TaskManager(App):
+    def build(self):
+        return Button(text='Button1')
+
+class Button(Button):
+    pass
+
+taskManager = TaskManager()
+
+taskManager.run()
